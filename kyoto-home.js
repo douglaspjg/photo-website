eb66decccd341739cfda116ebf62238b7c9251ec
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-03-25 00:34:18 -0400

    changed homescreen to index

--- kyoto-home.js
+++ kyoto-home.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+const images = document.querySelector('.images-list');
+
+Array.from(images).forEach(function(li) {
+    if (!li.child.classList.contains('curent-image') {
+        li.child.style.
+    }
+
+}
+
+)
+
+console.log(images);
