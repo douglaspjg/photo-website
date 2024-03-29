e42182a4c94ef53083230b7bc2ab79027dc0a6fc
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-03-28 22:08:47 -0400

    minimally working website

--- kyoto-home.js
+++ kyoto-home.js
@@ -1,14 +1,17 @@
-console.log("Hello World")
+const buttons = document.querySelectorAll('.button');
+console.log(buttons)
 
-const images = document.querySelector('.images-list');
-
-Array.from(images).forEach(function(li) {
-    if (!li.child.classList.contains('curent-image') {
-        li.child.style.
-    }
-
-}
-
-)
-
-console.log(images);
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+        const currentSlide = slides.querySelector("[data-current]")
+        let newIndex = [...slides.children].indexOf(currentSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+        slides.children[newIndex].dataset.current = true
+        delete currentSlide.dataset.current
+    })
+})
