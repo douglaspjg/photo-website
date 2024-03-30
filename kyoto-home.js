63b97d6b37455da794fe87469330989a1497ab50
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-03-30 18:28:04 -0400

    more polished, things are working more smoothly

--- kyoto-home.js
+++ kyoto-home.js
@@ -4,6 +4,7 @@ console.log(buttons)
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        // Update the pictures
         const slides = button
             .closest("[data-carousel]")
             .querySelector("[data-slides]")
@@ -13,5 +14,15 @@ buttons.forEach(button => {
         if (newIndex >= slides.children.length) newIndex = 0
         slides.children[newIndex].dataset.current = true
         delete currentSlide.dataset.current
+        // Update the captions
+        const descriptions = button
+            .closest("[data-carousel]")
+            .querySelector("[data-descriptions]")
+        const currentDescription = descriptions.querySelector("[data-current]")
+        let newJndex = [...descriptions.children].indexOf(currentDescription) + offset
+        if (newJndex < 0) newJndex = descriptions.children.length - 1
+        if (newJndex >= descriptions.children.length) newJndex = 0
+        descriptions.children[newJndex].dataset.current = true
+        delete currentDescription.dataset.current
     })
 })
