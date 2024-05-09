436f6b1c3690201eca0b153d889dcb99177a8716
Douglas Pineda Gutierrez <djp5@williams.edu>
2024-05-08 23:19:14 -0400

    added huge carousel to homescreen

---
+++ index.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button');
+console.log(buttons)
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        // Update the pictures
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+        const currentSlide = slides.querySelector("[data-current]")
+        let newIndex = [...slides.children].indexOf(currentSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+        slides.children[newIndex].dataset.current = true
+        delete currentSlide.dataset.current
+        
+        const indexDisplay = document.getElementById("indexDisplay");
+        indexDisplay.innerText = (newIndex + 1) + "/" + slides.children.length;
+    })
+})
